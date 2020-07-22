88127c527f95a0e9b54e3429a044066d700acf36
Fábio <fdsgoncalves@outlook.com>
2020-07-22 18:30:22 -0300

    Day 02

---
+++ Day_02/js/index.js
@@ -0,0 +1,13 @@
+const root = document.querySelector('#root')
+const textTitle = 'Marcosdid'
+const textSmall = 'Texto pequeno'
+const otherText = 'Outro texto'
+
+// Tagged Template String
+const title = Title`
+    color: red; font-size: 20px;
+    ${textTitle}
+`
+
+root.insertAdjacentHTML('beforeend', title)
+
